ebe29bcbcbeeb4f1c7b4d7630536e94c5499e625
Nathan Zadoks <nathan@nathan7.eu>
2013-05-04 10:54:59 +0200

    more correct testing ('in', not hasOwnProperty)

--- test.js
+++ test.js
@@ -2,6 +2,14 @@ var test = require('tape')
   , nullobj = require('./')
 
 test(function(t) {
-  t.notOk(Object.prototype.hasOwnProperty.call(nullobj(), 'hasOwnProperty'))
+  t.notOk('hasOwnProperty' in nullobj())
+  t.notOk('constructor' in nullobj())
+  t.notOk('hasOwnProperty' in nullobj())
+  t.notOk('propertyIsEnumerable' in nullobj())
+  t.notOk('isPrototypeOf' in nullobj())
+  t.notOk('toLocaleString' in nullobj())
+  t.notOk('toString' in nullobj())
+  t.notOk('valueOf' in nullobj())
+
   t.end()
 })
